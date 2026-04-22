ec334e60525e53ef7752dfe297eb09f25589c629
Pratheesha <pratheeshabenedict@gmail.com>
2026-04-22 12:15:58 +0530

    send email added

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,13 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+  service: 'Gmail',
+  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
 });
 
 exports.sendEmail = async ({ to, subject, html }) => {
-    await transporter.sendMail({
-        from: `"AI Writer" <${process.env.EMAIL_USER}>`,
-        to, subject, html,
-    });
+  await transporter.sendMail({
+    from: `"AI Writer" <${process.env.EMAIL_USER}>`,
+    to, subject, html,
+  });
 };
